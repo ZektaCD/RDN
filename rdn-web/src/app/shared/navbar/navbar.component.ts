610fee5890f640977dd91ecf658477b88fc0cdcf
Zekta <eze.atienza@hotmail.com>
2026-05-20 16:56:05 -0300

    home 1.8

--- rdn-web/src/app/shared/navbar/navbar.component.ts
+++ rdn-web/src/app/shared/navbar/navbar.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { faBars} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import 'animate.css';
 import 'hover.css';
 
 @Component({
